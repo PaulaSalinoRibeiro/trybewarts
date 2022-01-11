090aec50fa2ec1eed548459ccc8c16b11ef4336a
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-11 16:27:27 -0300

    Requisito 3 semi-concluido(erros no lint)

---
+++ script.js
@@ -0,0 +1,13 @@
+const email = document.querySelector('[name=email]');
+const password = document.querySelector('[name=password]');
+const btnLogin = document.getElementById('btn-login');
+
+function checkLogin() {
+    if (email.value === 'tryber@teste.com' && password.value === '123456') {
+        alert('Olá, Tryber!');
+    } else {
+        alert('Email ou senha inválidos.');
+    }
+}
+
+btnLogin.addEventListener('click', checkLogin);
