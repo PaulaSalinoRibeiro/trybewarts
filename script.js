aec2a24f8b53205db15288a36a2b6d8479650a7f
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-11 22:33:17 -0300

    Requisito 18 concluido

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.querySelector('[name=email]');
 const password = document.querySelector('[name=password]');
 const btnLogin = document.getElementById('btn-login');
+const btnSubmit = document.getElementById('submit-btn');
+const checkBox = document.getElementById('agreement');
 
 function checkLogin() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -11,3 +13,13 @@ function checkLogin() {
 }
 
 btnLogin.addEventListener('click', checkLogin);
+
+window.onload = function () {
+  btnSubmit.disabled = true;
+  function checkBoxVerifired() {
+    if (checkBox.checked === true) {
+      btnSubmit.disabled = false;
+    }
+  }
+  checkBox.addEventListener('click', checkBoxVerifired);
+};
