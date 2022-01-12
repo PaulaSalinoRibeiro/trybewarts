722e83fb31fad3afe714ab187632286dcb2b22c8
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-12 08:38:27 -0300

    Requisito bonus 20 concluido e erros do Lint corrigidos

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const password = document.querySelector('[name=password]');
 const btnLogin = document.getElementById('btn-login');
 const btnSubmit = document.getElementById('submit-btn');
 const checkBox = document.getElementById('agreement');
+const textArea = document.getElementById('text-area');
+const counter = document.getElementById('counter');
 
 function checkLogin() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -12,9 +14,10 @@ function checkLogin() {
   }
 }
 
-//Referência:https://cursos.alura.com.br/forum/topico-habitar-desabilitar-botao-65202
 btnLogin.addEventListener('click', checkLogin);
+
 window.onload = function () {
+  // Referência:https://cursos.alura.com.br/forum/topico-habitar-desabilitar-botao-65202
   btnSubmit.disabled = true;
   function checkBoxVerifired() {
     if (checkBox.checked === true) {
@@ -23,3 +26,11 @@ window.onload = function () {
   }
   checkBox.addEventListener('click', checkBoxVerifired);
 };
+
+function countCharacts() {
+  const characts = textArea.value;
+  const count = (500 - characts.length);
+  counter.innerText = count;
+}
+
+textArea.addEventListener('keyup', countCharacts);
