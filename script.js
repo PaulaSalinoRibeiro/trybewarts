9cc42f18da27f9e59f1f8d7f9abe3db18421986f
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-15 09:38:19 -0300

    Requisito 21 concluido

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const password = document.querySelector('[name=password]');
 const btnLogin = document.getElementById('btn-login');
 const btnSubmit = document.getElementById('submit-btn');
 const checkBox = document.getElementById('agreement');
-const textArea = document.getElementById('text-area');
+const textArea = document.getElementById('textarea');
 const counter = document.getElementById('counter');
+const fieldsForm = document.getElementById('evaluation-form');
 
 function checkLogin() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -35,4 +36,27 @@ function countCharacts() {
 
 textArea.addEventListener('keyup', countCharacts);
 
-// Requisito 21 > conteúdo dentro da tag form
+function learn() {
+  const material = document.querySelectorAll('.subject:checked'); // Dica de resolução monitoria Summer(David Gonzaga)
+  const mat = [];
+  for (let i = 0; i < material.length; i += 1) {
+    mat.push(material[i].value);
+  }
+  return mat.join(', ');
+}
+
+function checkInputs(event) { // Dica de resolução Diogo Pantaleão
+  event.preventDefault();
+  const paragph = document.createElement('p');
+  paragph.innerText = `Nome: ${fieldsForm['input-name'].value} ${fieldsForm['input-lastname'].value}
+                        Email: ${fieldsForm['input-email'].value}
+                        Casa: ${fieldsForm.house.value}
+                        Família: ${fieldsForm.family.value}
+                        Matérias: ${learn()}
+                        Avaliação: ${fieldsForm.rate.value}
+                        Observações: ${fieldsForm.textarea.value}`;
+  fieldsForm.innerHTML = '';
+  fieldsForm.appendChild(paragph);
+}
+
+btnSubmit.addEventListener('click', checkInputs);
